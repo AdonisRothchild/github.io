403e984fb1d3635813d67707a3f3f4c7d224d80e
adonis <a1121732664@outlook.com>
2020-06-18 22:43:14 +0800

    2020 14 06 to master

---
+++ kt-spring/src/main/kotlin/com/ad/controller/FirstController.java
@@ -0,0 +1,17 @@
+package com.ad.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 这是初步测试的代码Controller
+ */
+@RestController
+public class FirstController
+{
+    @GetMapping("sss")
+    public void test()
+    {
+
+    }
+}
